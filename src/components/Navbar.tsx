83dcb207f34fc34844f73d7386092578fdbf0fb9
yankarlosgv <josual0612@gmail.com>
2026-08-18 19:18:03 -0600

    Actualiza mi Proyecto

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,31 @@
-export const Navbar = () => { return (
-    <header className="header">
-    <div className="logo">
-        <h1>YANKA-STYLE</h1>
-    </div>
-    <nav className="nav-links">
-        <a href="#inicio">Inicio</a>
-        <a href="#servicios">Servicios</a>
-        <a href="#citas">Citas</a>
-        <a href="#contacto">Contacto</a>
-    </nav>
-    </header> );
+export const Navbar = () => {
+    return (
+        <header className="header">
+            <div className="logo">
+                <h1>YANKA-STYLE</h1>
+            </div>
+
+            <nav className="nav-links">
+                <a href="#inicio">Inicio</a>
+                <a href="#servicios">Servicios</a>
+                <a href="#citas">Citas</a>
+
+
+                <a
+                    href="https://www.instagram.com/yanka_stylee/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Instagram
+                </a>
+                <a
+                    href="https://web.whatsapp.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Contacto
+                </a>
+            </nav>
+        </header>
+    );
 };
